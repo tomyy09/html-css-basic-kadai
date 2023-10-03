6422645a18cbacf841e40332be89fdb9916ee9c5
tomyy09 <144043091+tomyy09@users.noreply.github.com>
2023-10-03 22:50:41 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+let num = Math.floor(Math.random() * 30);
+
+if(num % 3 == 0 && num % 5 == 0){
+  console.log('３と５の倍数です');
+}
+
+else if(num % 3 == 0){
+console.log('３の倍数です');
+}
+
+else if(num % 5 == 0){
+console.log('５の倍数です');
+}
+
+
+
+else{
+  console.log(num);
+}
