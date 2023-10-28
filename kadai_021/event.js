51bd0164dc9612d9d2fb32dd53ba947edbf07091
tomyy09 <144043091+tomyy09@users.noreply.github.com>
2023-10-29 00:47:22 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,9 +0,0 @@
-console.log(document.getElementById("text").textContent);
-
-btn.addEventListener('click',() => {
-  setTimeout(function() {
-    text.textContent = 'ボタンをクリックされました';
-  }, 2000);
-})
-
-
